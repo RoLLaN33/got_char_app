c38f5782d6b38ac0a800a1b21be80b0e37a68f2f
Rollan <antts11@gmail.com>
2022-05-26 17:26:59 +0300

    added character list and errors

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,16 +2,24 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
+import ErrorMessage from '../errorMessage';
+import CharacterPage from '../characterPage';
 
 import './app.css';
 
 export default class App extends Component {
    
     state = {
-       showRandomChar: true
+       showRandomChar: true,
+       error: false
    };
+
+   componentDidCatch() {
+       console.log('error');
+       this.setState({
+           error: true
+       })
+   }
 
    toggleRandomChar = () => {
        this.setState((state) => {
@@ -21,8 +29,14 @@ export default class App extends Component {
        });
    };
     
+
     render() {
         const char = this.state.showRandomChar ? <RandomChar/> : null
+
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+
         return (
             <> 
                 <Container>
@@ -38,14 +52,7 @@ export default class App extends Component {
                                 >RANDOM CHARACTER</button>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col md='6'>
-                            <ItemList />
-                        </Col>
-                        <Col md='6'>
-                            <CharDetails />
-                        </Col>
-                    </Row>
+                    <CharacterPage/>
                 </Container>
             </>
         );
