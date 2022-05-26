5edec37faa3b23283967555a1902cb5ff52ca6d4
Rollan <antts11@gmail.com>
2022-05-26 15:36:00 +0300

    random character

---
+++ src/components/app/app.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {Col, Row, Container} from 'reactstrap';
+import Header from '../header';
+import RandomChar from '../randomChar';
+import ItemList from '../itemList';
+import CharDetails from '../charDetails';
+
+import './app.css';
+
+export default class App extends Component {
+   
+    state = {
+       showRandomChar: true
+   };
+
+   toggleRandomChar = () => {
+       this.setState((state) => {
+           return {
+               showRandomChar: !state.showRandomChar
+           }
+       });
+   };
+    
+    render() {
+        const char = this.state.showRandomChar ? <RandomChar/> : null
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {char}
+                            <button 
+                                className='btn_random'
+                                onClick={this.toggleRandomChar}
+                                >RANDOM CHARACTER</button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+};
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-pprvms087f9">
+        <div class="ldio-y5aopu7pe68">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
